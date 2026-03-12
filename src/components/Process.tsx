e6838c2a2e8b527b9a7b7db6284159b428f35c86
Ereh <erehuchiha00@gmail.com>
2026-03-13 01:22:51 +0530

    feat: successfully added the Google Analytics tag to your index.html

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -34,7 +34,7 @@ export default function Process() {
           </h3>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {steps.map((step, index) => (
             <motion.div
               key={index}
